c5fdb018eeec641a1102e6d0775bd314fb235c93
zhang1024899756 <zxy_email@qq.com>
2020-11-18 14:40:37 +0800

    初始化构建 完成基本组件-按钮

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h4>按钮组件</h4>
+    <div class="demo-p">
+      <Button>默认按钮</Button>
+      <Button type='primary'>默认按钮</Button>
+      <Button type='success'>默认按钮</Button>
+      <Button type='warning'>默认按钮</Button>
+      <Button type='danger'>默认按钮</Button>
+      <Button type='default'>默认按钮</Button>
+      <Button size="hg">大型按钮</Button>
+      <Button size="lg">中型按钮</Button>
+      <Button size="sm">小型按钮</Button>
+      <Button size="mini">迷你按钮</Button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+import Button from './components/Button/index.vue'
+
+@Options({
+  components: {
+    Button
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+.demo-p{
+  padding: 10px;
+  > * {
+    margin-right: 10px;
+  }
+}
+</style>
---
+++ src/components/Button/index.vue
@@ -0,0 +1,75 @@
+<template>
+  <button
+    :class="[
+      'mo-button',
+      type ? 'mo-button--' + type : '',
+      buttonSize ? 'mo-button--' + buttonSize : '',
+      {
+        'disabled': buttonDisabled
+      }
+    ]"
+    :disabled="buttonDisabled || loading"
+    :autofocus="autofocus"
+    :type="nativeType"
+    @click="handleClick"
+  >
+    <span v-if="$slots.default"><slot></slot></span>
+  </button>
+</template>
+
+<script lang='ts'>
+import { computed, defineComponent } from 'vue'
+
+import type { PropType } from 'vue'
+
+type IButtonType = PropType<'primary' | 'success' | 'warning' | 'danger' | 'default'>
+type IButtonSize = PropType<'hg'|'lg'|'sm'|'mini'>
+type IButtonNativeType = PropType<'button' | 'submit' | 'reset'>
+type EmitFn = (evt: Event) => void
+
+interface ButtonProps {
+  type: string;
+  size: string;
+  nativeType: string;
+  loading: boolean;
+  disabled: boolean;
+  autofocus: boolean;
+}
+export default defineComponent({
+  name: 'MoButton',
+  props: {
+    type: {
+      type: String as IButtonType,
+      default: 'default',
+      validator: (val: string) => {
+        return ['default', 'primary', 'success', 'warning', 'danger'].includes(val)
+      }
+    },
+    size: {
+      type: String as IButtonSize,
+      validator: (val: string) => ['hg', 'lg', 'sm', 'mini'].includes(val)
+    },
+    nativeType: {
+      type: String as IButtonNativeType,
+      default: 'button',
+      validator: (val: string) => {
+        return ['button', 'submit', 'reset'].includes(val)
+      }
+    },
+    loading: Boolean,
+    disabled: Boolean,
+    autofocus: Boolean
+  },
+  emits: ['click'],
+  setup (props, ctx) {
+    const buttonSize = computed(() => {
+      return props.size
+    })
+    // methods
+    const handleClick = (evt: any) => {
+      ctx.emit('click', evt)
+    }
+    return { buttonSize, handleClick }
+  }
+})
+</script>
